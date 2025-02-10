f4cb7ac94f4345110008285e46623a0ab355163f
monsoonification <austraeohperhaps@gmail.com>
2025-02-09 18:24:24 -0600

    holy bingle working web server

--- src/main_file_writing.rs
+++ src/main_file_writing.rs
@@ -1,19 +1,21 @@
+extern crate actix_web; 
 extern crate chrono;
 
 use std::io::prelude::*;
 use std::fs::{File,OpenOptions};
-// use std::fs::File;
 use std::io;
+use actix_web::cookie::time::ext;
+use actix_web::{web, App, HttpServer, Responder};
 use chrono::*;
 
 
-fn log_something(filename: &'static str, string: &'static [u8; 12]) -> io::Result<()> {
-    let mut f = File::create(filename)?;
-    (f.write_all(string))?;
-    Ok(())
-}
+// fn log_something(filename: &'static str, string: &'static [u8; 12]) -> io::Result<()> {
+//     let mut f = File::create(filename)?;
+//     (f.write_all(string))?;
+//     Ok(())
+// }
 
-fn log_time_entry() -> String {
+fn formatted_time_entry() -> String {
     let local: DateTime<Local> = Local::now();
     let formatted = local.format("%Y-%m-%d %H:%M:%S").to_string();
     formatted
@@ -29,17 +31,33 @@ fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
     Ok(())
 }
 
-fn log_time(filename: &'static str) -> io::Result<()> {
-    let entry = log_time_entry();
+async fn log_time() -> impl Responder {
+    let entry = formatted_time_entry();
     let entry_bytes = entry.as_bytes();
-    record_entry_in_log(filename, entry_bytes)?;
-    Ok(())
+    if let Err(e) = record_entry_in_log("log.txt", &entry_bytes) {
+        return format!("Error: {}", e);
+    }
+    format!("Entry logged!: {}", entry)
 }
 
+// fn do_log_time() -> String {
+//     match log_time("log.txt") {
+//         Ok(entry) => format!("Entry logged!: {}", entry),
+//         Err(e) => format!("Error: {}", e)
+//     }
+// }
 
-fn main() {
-    match log_time("log.txt") {
-        Ok(..) => println!("File created!"),
-        Err(e) => println!("Error: {}", e)
-    }
+async fn do_log_time() -> impl Responder {
+    "Logging time..."
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(log_time))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
